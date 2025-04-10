f7ef64301099677ddb5c50a06d3f548ea49fa846
dhruv <dhruv.chatrani@gmail.com>
2025-04-10 16:49:26 +0530

    added port to fallback

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # <-- Dynamic port for Render
+    app.run(host="0.0.0.0", port=port)
